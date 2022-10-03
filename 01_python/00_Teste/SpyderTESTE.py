34e1d0b56dbff56d798980715954d27354dfabf5
CAIO LIMA SOUSA <caiolgn88@gmail.com>
2022-10-02 22:24:29 -0300

    primeiro commit

---
+++ 01_python/00_Teste/SpyderTESTE.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 09:16:21 2021
+
+@author: caiol
+"""
+
+# Importando as Libs
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+plt.rcParams['figure.figsize'] = [12,8]
